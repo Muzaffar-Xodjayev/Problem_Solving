929d05929fc2f619b3200d47214c065a2fbde84e
Muzaffar <khodjayevmuzaffar@gmail.com>
2025-08-07 12:15:40 +0500

    solve another problem

--- main.py
+++ main.py
@@ -17,12 +17,29 @@
 # print(Solution().searchInsert(nums=[1,3,5,6], target=5))
 
 
-
 # https://leetcode.com/problems/length-of-last-word/
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip().rstrip()
-        s = s.split()
-        return len(s[-1])
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         s = s.strip().rstrip()
+#         s = s.split()
+#         return len(s[-1])
+#
+# print(Solution().lengthOfLastWord(s="   Hello   World    "))
 
-print(Solution().lengthOfLastWord(s="   Hello   World    "))
+
+# https://leetcode.com/problems/plus-one/
+from typing import List
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) -1, -1, -1):
+            if digits[i] + 1 != 10:
+                digits[i] = digits[i] + 1
+                return digits
+
+            digits[i] = 0
+            if i == 0:
+                return [1] + digits
+        return digits
+
+
+print(Solution().plusOne(digits=[9, 9]))
